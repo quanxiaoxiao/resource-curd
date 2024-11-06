b2159750a76f2dc65b4a92359c1234b01ccea76b
quan <quan_xiaoxiao@qq.com>
2024-11-06 13:42:38 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -2,9 +2,11 @@ import readImages from './readImages.mjs';
 import isImageOfPathname from './isImageOfPathname.mjs';
 import readResourceBufOfPathname from './readResourceBufOfPathname.mjs';
 import readImageBufOfPathname from './readImageBufOfPathname.mjs';
+import readResources from './readResources.mjs';
 
 export {
   readImages,
+  readResources,
   isImageOfPathname,
   readResourceBufOfPathname,
   readImageBufOfPathname,
